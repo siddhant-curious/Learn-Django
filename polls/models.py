9884bf2d45bb9bbb56a0d1418e7f9a227bc4b2c8
Siddhant <siddhant@Siddhants-MacBook-Air.local>
2019-04-02 23:57:24 +0530

    Added Question & Choices Models

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=180)
+    pub_date = models.DateTimeField()
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question,on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=180)
+    votes = models.IntegerField(default=0)
