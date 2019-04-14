b4cc1d7a773c22b4ae80ad7a0f36aeb0fdbeceed
siddhant-curious <siddhant1042@gmail.com>
2019-04-14 20:31:03 +0530

    Lesson 3 Views Completed

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='index'),
     path('spiderman',views.spiderman,name='spiderman'),
@@ -9,6 +10,6 @@ urlpatterns = [
     path('antman',views.antman),
     path('antman/',views.endingtrail),
     path('<int:question_id>/results',views.results,name='results'),
-    path('<int:question_id>',views.detail,name='question'),
+    path('<int:question_id>/',views.detail,name='detail'),
     path('<int:question_id>/vote',views.vote,name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
+from django.http import Http404
 # Create your views here.
 from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context,request))
+    # return render(request, 'polls/index.html', context)
 
 def spiderman(request):
     return HttpResponse("Spiderman does whatever a spider can")
@@ -21,7 +28,17 @@ def endingtrail(request):
     return HttpResponse("ending trail makes all the difference")
 
 def detail(request,question_id):
+    try:
+        quest = Question.objects.get(pk=question_id)
+        context = {'question':quest}
+        return render(request,'polls/question.html',context)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+
     return HttpResponse("You're looking at question %s. " % question_id)
+    # question = get_object_or_404(Question, pk=question_id)
+    # return render(request, 'polls/detail.html', {'question': question})
+
 
 def results(request,question_id):
     return HttpResponse("You're looking at results of question %s. " % question_id)
