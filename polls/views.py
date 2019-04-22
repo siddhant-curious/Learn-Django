39dd01f1e3d60c06f0ebe59fcf1182b585492299
Siddhant Chhabra <siddhant1042@gmail.com>
2019-04-22 19:06:59 +0530

    Forms and CRUD

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from django.template import loader
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import Http404
+from django.urls import reverse
 # Create your views here.
 from .models import Question
 
@@ -13,6 +14,8 @@ def index(request):
         'latest_question_list': latest_question_list,
     }
     return HttpResponse(template.render(context,request))
+    #It’s a very common idiom to load a template, fill a context and return an HttpResponse object with the result of the rendered template.
+    # Django provides a shortcut as
     # return render(request, 'polls/index.html', context)
 
 def spiderman(request):
@@ -28,20 +31,28 @@ def endingtrail(request):
     return HttpResponse("ending trail makes all the difference")
 
 def detail(request,question_id):
-    try:
-        quest = Question.objects.get(pk=question_id)
-        context = {'question':quest}
-        return render(request,'polls/question.html',context)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-
-    return HttpResponse("You're looking at question %s. " % question_id)
-    # question = get_object_or_404(Question, pk=question_id)
-    # return render(request, 'polls/detail.html', {'question': question})
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request,question_id):
-    return HttpResponse("You're looking at results of question %s. " % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request,question_id):
-    return HttpResponse("You're looking at vote of question %s. " % question_id)
+    question = get_object_or_404(Question,pk=question_id)
+    try:
+        selected_choice = question.answer_set.get(pk=request.POST['choice'])
+    except (KeyError, Answer.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
